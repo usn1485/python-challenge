89148c9e8676115efe341ecd1b81a7fc661c8b61
Ujwala <contact.uju@gmail.com>
2019-05-01 12:09:47 -0500

    Calculated Max and min From Change column

--- pyBank/main.py
+++ pyBank/main.py
@@ -3,6 +3,10 @@ import csv
 
 # Path to collect data from the Resources folder
 budgetData = os.path.join('budget_data.csv')
+#budgetData = os.path.join('C:/Users/Ujwala/Desktop/Washu/Python/python-challenge/pyBank/budget_data.csv')
+#Get avarage of the changes in profit/losses 
+
+
 
 # Read from the CSV file
 with open(budgetData, 'r') as csvfile:
@@ -12,21 +16,45 @@ with open(budgetData, 'r') as csvfile:
 
     header = next(csvreader,None)
     count=0
+    count1=0
     totalAmount=0
+    totalchange=0
+    track_change=0
+    Month=""
+    changeInPL=[]
+    pl_List=[]
+
     Months=['Jan','Feb','Mar','Apr','May','Jun','Jul',
-     'Aug','Sept','Oct','Nov','Dec']
-
-    Month=[]
-
+     'Aug','Sep','Oct','Nov','Dec']
+       
     for row in csvreader:
-        print(row)    
+          
         date=((row[0].split('-')))
-        #Month.append(date[3])
-        Month= (date[0]) 
+        Month= (date[0])
+         
         if Month in Months:
-            count= count+1
+            count= count+1    
             totalAmount= totalAmount + int(row[1])
-    
-    print(f'count is: {count}')
-    print(f'total amount is :{totalAmount}')
+        pl_List.append(row[1])
+        
+    for i in range(len(pl_List)-1):
+        count1= count1+1
+        track_change=(int(pl_List[i+1])-int(pl_List[i]))
+        changeInPL.append(track_change)
+        totalchange=totalchange + int(changeInPL[i])
+                
+    print(f'Length of pl :{len(pl_List)}')  
+    print(f'Total Change:{totalchange}')
+    print(f'Total count:{count1}')   
+    avarage = float(totalchange/count1)
+   
+    print('----------------------------')        
+    print ("Finanacial Analysis")
+    print('----------------------------')
+    print(f'Total Months:  {count}')
+    print(f'Total: ${totalAmount}')
+    print(f'Avarage Change:' "%.2f" % avarage)
+    print(f'Greatest Increase :{max(changeInPL)}')
+    print(f'Greatest Decrease :{min(changeInPL)}')
+
 
